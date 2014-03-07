c04f479b4fb73171848acb1c4335104bb9106160
Richard <richard@tinyrock.com>
2014-03-07 22:43:16 +1100

    Update nmea_to_xml.py

--- nmea_to_xml.py
+++ nmea_to_xml.py
@@ -1,5 +1,5 @@
 """
-    Copyright (C) 2014 Richard Laugesen, richard@tinyrock.com
+    Copyright (C) 2014 Tiny Rock Pty Ltd
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
